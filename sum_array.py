22c4428a992ccac0ff7629893f208044095b3589
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-18 21:08:02 +0300

    Sum of array

---
+++ sum_array.py
@@ -0,0 +1,7 @@
+# Write a program that returns the sum of an array [1, 2, 3, 4, 5].
+
+def _sum(array_elements):
+    return(sum(array_elements))
+
+arr = [2, 8, 6, 3, 9, 23, 20]
+print('Sum of an array of elements is: {}'.format(_sum(arr)))
